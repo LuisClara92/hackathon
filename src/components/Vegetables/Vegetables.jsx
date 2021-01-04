0db4a84f97cb5623d968ed07cf766e6bf76c0d11
LuisClara92 <lc.sbg.92@gmail.com>
2021-01-04 22:24:16 +0000

    all conflicts resolve for deploy

--- src/components/Vegetables/Vegetables.jsx
+++ src/components/Vegetables/Vegetables.jsx
@@ -7,14 +7,14 @@ import { NavLink as Link} from "react-router-dom";
 import './styles.css';
 
 const Goods = () => {
-  const [goods, setGoods] = useState([]);
-  
-  useEffect(() => {
-    axios
-      .get(`https://run.mocky.io/v3/fa107af3-65bc-406c-830e-1003fd542f0c`)
-      .then((response) => setGoods(response.data.results))
+    const [goods, setGoods] = useState([]);
+    
+    useEffect(() => {
+      axios
+        .get(`https://run.mocky.io/v3/fa107af3-65bc-406c-830e-1003fd542f0c`)
+        .then((response) => setGoods(response.data.results))
 
-  }, []);
+    }, []);
 
   return (
        
